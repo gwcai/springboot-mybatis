c4ea418653e1f01a2719b8aeb1c15ecdebd02317
gaowc <lili14520@gmail.com>
2019-07-12 17:36:39 +0800

    init

---
+++ src/main/java/com/spring/base/SpringbootMybatisApplication.java
@@ -0,0 +1,16 @@
+package com.spring.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author FUDIAN
+ */
+@SpringBootApplication
+public class SpringbootMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisApplication.class, args);
+    }
+
+}
